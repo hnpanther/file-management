d147117f9a4f0afb1bc2e9b60b8a04b88acaf2da
hnpanther <hadi.hnp@gmail.com>
2023-12-28 13:02:27 +0330

    change database schema

--- src/main/resources/db/migration/V1__Initial_Setup.sql
+++ src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -1,17 +1,39 @@
 CREATE TABLE file_category(
                               id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                               category_name VARCHAR(100) NOT NULL,
-                              description VARCHAR(100),
+                              description VARCHAR(1000),
+                              path VARCHAR(100) NOT NULL,
                               CONSTRAINT uq_file_category_category_name UNIQUE(category_name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE file_sub_category(
+                                  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                                  sub_category_name VARCHAR(100) NOT NULL,
+                                  file_category_id INT NOT NULL,
+                                  description VARCHAR(1000),
+                                  path VARCHAR(100) NOT NULL,
+                                  CONSTRAINT fk_file_sub_category_file_category_id FOREIGN KEY(file_category_id) REFERENCES file_category(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE file_info(
+                          id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                          file_name VARCHAR(100) NOT NULL,
+                          description VARCHAR(1000),
+                          file_path VARCHAR(1000) NOT NULL,
+                          file_link VARCHAR(1000),
+                          file_sub_category_id INT NOT NULL,
+                          CONSTRAINT fk_file_details_file_sub_category_id FOREIGN KEY(file_sub_category_id) REFERENCES file_sub_category(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE file_details(
                              id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                             file_info_id INT NOT NULL,
                              file_name VARCHAR(100) NOT NULL,
                              file_extension VARCHAR(10) NOT NULL,
+                             version INT NOT NULL,
                              description VARCHAR(1000),
                              file_path VARCHAR(1000) NOT NULL,
                              file_link VARCHAR(1000),
-                             file_category_id INT NOT NULL,
-                             CONSTRAINT fk_file_details_file_category_id FOREIGN KEY(file_category_id) REFERENCES file_category(id)
+                             file_size INT NOT NULL,
+                             CONSTRAINT fk_file_details_file_info_id FOREIGN KEY(file_info_id) REFERENCES file_info(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
